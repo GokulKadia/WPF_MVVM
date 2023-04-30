4cfb95a7e02e60bb3c57591477a452149ea4ac85
gokul_kadia <gokul.kadia@dellteam.com>
2023-04-30 16:04:42 +0530

    WPF MVVM

---
+++ WPF_MVVM/Command/CustomCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPF_MVVM.Command
+{
+    public class CustomCommand : ICommand
+    {
+        private Action<object> _Execute;
+        private Predicate<object> _CanExecute;
+        private ICommand setCacheSize;
+        private object p;
+        private ICommand removeAFolderFromCleanup;
+        private Func<object, bool> canRemoveAFolderFromCleanup;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _CanExecute == null ? true : _CanExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _Execute(parameter);
+        }
+
+        public CustomCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _Execute = execute;
+            _CanExecute = canExecute;
+        }
+
+      
+
+      
+    }
+}
---
+++ WPF_MVVM/ViewModel/TestViewVM.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WPF_MVVM.Command;
+
+namespace WPF_MVVM.ViewModel
+{
+    public class data
+    {
+        public string  Name { get; set; }
+        public string Surname { get; set; }
+    }
+    public class TestViewVM
+    {
+        public ICommand ClickCommand { get;private set; }
+
+        public data _selected;
+        public data Selectedcmd
+        {
+            get { return _selected; }
+            set
+            {
+                _selected = value;
+            }
+        }
+        public string LName { get; set; }
+        // public ObservableCollection<data> data = new ObservableCollection<data>();
+        public ObservableCollection<data> data { get; set; }
+        public TestViewVM()
+        {
+            ClickCommand = new CustomCommand(ClickCmd, null);
+            data = new ObservableCollection<data>();
+            data.Add(new data { Name = "Gokul",Surname="Kadia" }) ;
+            LName = "Kadia Gokul";
+
+        }
+        private void ClickCmd(object obj)
+        {
+            data.Add(Selectedcmd);
+        }
+
+    }
+}
